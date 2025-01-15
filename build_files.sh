4eebdd315a8443c12e798874a90f7798221e489c
Pramod Tejra <99544644+PramodTejra@users.noreply.github.com>
2025-01-16 01:07:12 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,9 @@
-echo " BUILD START"
+echo "BUILD START"
+
+# Install dependencies
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput-clear
-echo " BUILD END"
+
+# Collect static files
+python3.9 manage.py collectstatic --noinput --clear
+
+echo "BUILD END"
